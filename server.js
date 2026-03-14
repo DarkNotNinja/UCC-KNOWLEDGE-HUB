a230ac9e99c1f1c98e89f1ffa8862c014196d36f
DarkNotNinja <dylonpaulm@gmail.com>
2026-03-13 21:39:02 -0500

    Initial commit - UCC Knowledge Hub Server

--- server.js
+++ server.js
@@ -34,10 +34,11 @@ function authenticateToken(req, res, next) {
 
 // DB SETUP (WORKING)
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL
-    ? { rejectUnauthorized: false }
-    : false
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 // SIGNUP (WORKING)
